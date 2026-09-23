104e87c165e3ff811b4dd50e876fb1e8079e047e
mike-kopp01 <mkoppelmann2022@fau.edu>
2026-09-23 15:28:29 -0400

    Add test script for LangChain with ChatOpenAI

---
+++ homeworks/Homework2/LangChain02/test.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+llm = ChatOpenAI(model="gpt-4o-mini")
+print(llm.invoke("Say hello in one sentence.").content)
